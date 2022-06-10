0a422392007f814f9a7519e36ceb87bbfe53760a
Warren Tran <warrentran2015@hotmail.com>
2022-06-10 14:39:01 -0400

    descriptive message

---
+++ Cars.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace CarClass
+{
+    public class Cars
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public void Description()
+        {
+            Console.WriteLine($"The {Model} was made in {Year} and was made by Warren{Make}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CarClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Cars();
+            car.Make = "Warren Tran";
+            car.Model = "Honda";
+            car.Year = 2017;
+
+            car.Description();
+        }
+    }
+}
